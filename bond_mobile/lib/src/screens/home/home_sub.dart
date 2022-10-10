a87363c1ce47f7133c0d7607858d640b2bde3dee
Jael-dev <jaelngaleu@gmai.com>
2022-10-10 13:53:10 +0100

    Avatar and couple name done

--- bond_mobile/lib/src/screens/home/home_sub.dart
+++ bond_mobile/lib/src/screens/home/home_sub.dart
@@ -25,17 +25,53 @@ class _HomeSubState extends ConsumerState<HomeSub> {
               style: Styles.heading3.copyWith(fontSize: 30),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical:8.0),
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Container(
                 height: 5,
-                width: context.screenWidth*0.85,
+                width: context.screenWidth * 0.85,
                 decoration: Decorations.transperentButton.copyWith(
-                  border: Border.all(color: Palette.orange, width: 1),
-                  color: Palette.orange
+                    border: Border.all(color: Palette.orange, width: 1),
+                    color: Palette.orange),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12.0),
+              child: SizedBox(
+                width: context.screenWidth,
+                height: context.screenHeight / 5.5,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: context.screenWidth * 0.2,
+                      child: const CircleAvatar(
+                        radius: 70,
+                        backgroundImage: NetworkImage(
+                            "https://image.shutterstock.com/image-photo/close-portrait-handsome-young-black-260nw-220562620.jpg"),
+                      ),
+                    ),
+                    Positioned(
+                      left: context.screenWidth * 0.45,
+                      child: const CircleAvatar(
+                        radius: 70,
+                        backgroundImage: NetworkImage(
+                            "https://braperucci.africa/wp-content/uploads/2019/03/Jare-braperucci.africa-819x1024.jpg"),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            
+            Container(
+              height: 40,
+              width: context.screenWidth / 1.8,
+              decoration: Decorations.transperentButton.copyWith(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Palette.orange, width: 1),
+                  color: Palette.orange
+                  ),
+              child: Center(child: Text("Les Tourtereaux", style: Styles.heading1.copyWith(color: Palette.white,fontSize: 20))),
+            )
           ],
         ))
       ],
